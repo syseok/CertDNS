c3f67ffc0cab70eb2f0ff61e9f42e1f34b8b659a
Ubuntu <ubuntu@ip-172-31-4-182.ap-northeast-2.compute.internal>
2022-02-03 02:03:57 +0000

    Certificate check

--- CertDNSProxy.py
+++ CertDNSProxy.py
@@ -42,12 +42,12 @@ def validate(dns_record,DNSserverIP):
 	#find cache first
 	domain_name = str(dns_record.questions[0].qname)
 
-	print("============DNS record (TXT)==============")
+	# print("============DNS record (TXT)==============")
 	txt_q = dnslib.DNSRecord.question(dns_record.questions[0].qname,"TXT")
 	# print(txt_q.pack())
 	txt_q_time = time.time()
 	TCPanswer = sendTCP(DNSserverIP, txt_q.pack())
-	print("TXT query time :",time.time()-txt_q_time)
+	# print("TXT query time :",time.time()-txt_q_time)
 
 
 	if(TCPanswer):
@@ -58,7 +58,7 @@ def validate(dns_record,DNSserverIP):
 		if (int(rcode, 16) == 1):
 			print ("Request is not a DNS query. Format Error!")
 		else:
-			print ("Success!")
+			# print ("Success!")
 
 			signaturegentime = time.time()
 
@@ -78,19 +78,19 @@ def validate(dns_record,DNSserverIP):
 
 			signature=signature_l[0]+signature_l[1]
 
-			print("signature generation :",time.time()-signaturegentime)
+			# print("signature generation :",time.time()-signaturegentime)
 			
 			base64time = time.time()
 			sig_byte = bytes(signature,'utf-8')
 			sig_to_verify = base64.b64decode(sig_byte)
 
-			print("base64 decode time :",time.time()-base64time)
+			# print("base64 decode time :",time.time()-base64time)
 
 			sha256time = time.time()
 			record_h = bytes(str(dns_record.rr[0].rdata),'utf-8')
 			target_h=SHA256.new(record_h)
 
-			print("SHA256time :",time.time()-sha256time)			
+			# print("SHA256time :",time.time()-sha256time)			
 			
 	else:
 		print("Signature get Fail")
@@ -98,7 +98,7 @@ def validate(dns_record,DNSserverIP):
 
 	
 	if(cert_loc in key_cache_dict):
-		print("cached!")
+		# print("cached!")
 		publickey_path = key_cache_dict[cert_loc]
 
 
@@ -106,13 +106,13 @@ def validate(dns_record,DNSserverIP):
 	##not cached
 	## get CERT record for PublicKey
 
-		print("============DNS record (CERT)==============")
+		# print("============DNS record (CERT)==============")
 		time_cert = time.time()
 
 		cert_q = dnslib.DNSRecord.question(".".join(str(dns_record.questions[0].qname).split(".")[1:]),"CERT")
 		TCPanswer = sendTCP(DNSserverIP, cert_q.pack())
 
-		print("CERT q time :",time.time()-time_cert)
+		# print("CERT q time :",time.time()-time_cert)
 
 		if(TCPanswer):
 			rcode = codecs.encode(TCPanswer[:6],'hex')
@@ -123,7 +123,7 @@ def validate(dns_record,DNSserverIP):
 			else:
 				
 				time_to_file = time.time()
-				print ("Success!")
+				# print ("Success!")
 				UDPanswer = TCPanswer[2:]
 				txt_dd = dnslib.DNSRecord.parse(UDPanswer)
 				pkiiii = txt_dd.rr[0].rdata.toZone()
@@ -134,22 +134,27 @@ def validate(dns_record,DNSserverIP):
 					text_file.write("%s\n" % pemstring)
 					text_file.write("-----END CERTIFICATE-----")
 
-				print("time cert to file :",time.time()-time_to_file)
+				# print("time cert to file :",time.time()-time_to_file)
 
 				validationtime = time.time()
 				try:
 
 					sig_ret = subprocess.check_output("openssl verify -CAfile isrgrootx1.pem -untrusted lets-encrypt-r3.pem "+cert_loc+'.pem',shell=True)
-					print("@@ SIG VAL RESULT :",str(sig_ret[-3:]))
+					# print("@@ SIG VAL RESULT :",str(sig_ret[-3:]))
 				
 					sig_cname = subprocess.check_output("openssl x509 -noout -subject -in "+cert_loc+'.pem',shell=True)
-					print(sig_cname)
+					# print(sig_cname)
 
 				except(Exception):
 					print("Certificate validation Fail!!!")
 					return
 
-				print("CERT validation time :",time.time()-validationtime)
+				# if((".".join(str(dns_record.questions[0].qname).split(".")[1:-1])) in str(sig_cname)):
+				# 	print("Certificate for",sig_cname,(".".join(str(dns_record.questions[0].qname).split(".")[1:-1])))
+				# else:
+				# 	print("NO@@@@@@@@@@@@@@@@",sig_cname,".".join(str(dns_record.questions[0].qname).split(".")[1:-1]))
+
+				# print("CERT validation time :",time.time()-validationtime)
 
 				pubkeyouttime = time.time()
 				os.system('openssl x509 -pubkey -noout -in '+cert_loc+'.pem > '+ cert_loc+'_pubkey.pem')
@@ -157,27 +162,27 @@ def validate(dns_record,DNSserverIP):
 				
 				publickey_path = cert_loc+'_pubkey.pem'
 				key_cache_dict[cert_loc] = publickey_path
-				print("Pubkey from CERT time :",time.time()-pubkeyouttime)
+				# print("Pubkey from CERT time :",time.time()-pubkeyouttime)
 
 		else:
 			print("Certificate Get Fail")
 
 
-	print("============Validation result==============")
+	# print("============Validation result==============")
 	siganaturevalidationtime = time.time()
 	
-	print(publickey_path)
+	# print(publickey_path)
 	ff = open(publickey_path,'r+b')
-	print("open key time :",time.time()-siganaturevalidationtime)
+	# print("open key time :",time.time()-siganaturevalidationtime)
 	siganaturevalidationtime = time.time()
 
 	public_key = RSA.importKey(ff.read())
-	print("importKey time :",time.time()-siganaturevalidationtime)
+	# print("importKey time :",time.time()-siganaturevalidationtime)
 	siganaturevalidationtime = time.time()
 
 	pkcs1_15.new(public_key).verify(target_h,sig_to_verify)
-	print("signaturevalidation time :",time.time()-siganaturevalidationtime)
-	print("@#$@  valid  #$@#$@  valid  #$@#$  valid  @#$@#$  valid  @#$#@$  valid  #$#@")
+	# print("signaturevalidation time :",time.time()-siganaturevalidationtime)
+	# print("@#$@  valid  #$@#$@  valid  #$@#$  valid  @#$@#$  valid  @#$#@$  valid  #$#@")
 	ff.close()
 
 	
@@ -198,26 +203,26 @@ def handler(data, addr, socket, DNSserverIP):
 		if (int(rcode, 16) == 1):
 			print ("Request is not a DNS query. Format Error!")
 		else:
-			print ("Success!")
-			print("A record time :", time.time()-start_handler_time)
+			# print ("Success!")
+			# print("A record time :", time.time()-start_handler_time)
 			UDPanswer = TCPanswer[2:]
 			dd = dnslib.DNSRecord.parse(UDPanswer)
-			print("============DNS record (A)==============")
-			print(dd)
+			# print("============DNS record (A)==============")
+			# print(dd)
 			
-			print("============DNS record (A) end==============")
+			# print("============DNS record (A) end==============")
 
 			#if validation is needed
-			needed=True
-			# needed=False
+			# needed=True
+			needed=False
 
 			if(needed):
 				validate(dd,DNSserverIP)
 
 			socket.sendto(UDPanswer, addr)
-			print("##########  time  ############")
+			# print("##########  time  ############")
 			print(time.time()-start_handler_time)
-			print("########## time end #########")
+			# print("########## time end #########")
 
 	else:
 		print ("Request is not a DNS query. Format Error!")
@@ -238,7 +243,7 @@ if __name__ == '__main__':
 
 		data,addr = sock.recvfrom(10240)
 
-		print(addr)
+		# print(addr)
 
 		_thread.start_new_thread(handler, (data, addr, sock, DNSserverIP))
 
